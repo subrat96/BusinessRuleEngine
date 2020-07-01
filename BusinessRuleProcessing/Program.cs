8907e08d0aaf4a28f9ac761051ad047337f3aced
Behera, Subrat <sbehera@itron.com>
2020-07-01 21:20:57 +0530

    added a simple .net core application

---
+++ BusinessRuleProcessing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BusinessRuleProcessing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
